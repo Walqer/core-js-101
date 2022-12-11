f574b559840dcd90d57111889626c7f9de6d01da
Alimossim <agaev.alimosim@gmail.com>
2022-12-11 22:57:42 +0600

    feat: solve string task

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
   },
   rules: {
-    "linebreak-style": ["error", "windows"]
+    "linebreak-style": ["error", "windows"],
+    "no-plusplus": 0,
   },
 };
